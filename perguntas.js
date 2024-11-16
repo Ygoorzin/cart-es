c9410ff01b34b1acbc57e4a47f40149062250e12
Ygor <00001105145141sp@al.educacao.sp.gov.br>
2024-11-16 02:38:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    categoria = 'Programação',
+    pergunta = 'O que é JavaScript?',
+    resposta = 'O JavaScript é uma linguagem de programação'
+)
+
+criaCartao(
+    categoria = 'Programação',
+    pergunta = 'O que é CSS?',
+    resposta = 'O CSS é uma linguagem de estilização'
+)
+
+criaCartao(
+    categoria = 'Programação',
+    pergunta = 'O que é Html?',
+    resposta = 'O Html é linguagem de marcação de hipertexto'
+)
+
+criaCartao(
+    categoria = 'Geografia',
+    pergunta= 'Qual a capital da França?',
+    resposta= 'A capital da França é Paris'
+)
+
+criaCartao(
+    categoria= 'Geografia',
+    pergunta= 'Qual a capital da Coreia do Sul?',
+    resposta= 'A capital da Coreia do Sul é Seul'
+)
+     
+criaCartao(
+    categoria = 'Geografia',
+    pergunta = 'Qual a capital da Croacia',
+    resposta = 'A capital da Croacia é Zagrebe'
+)
